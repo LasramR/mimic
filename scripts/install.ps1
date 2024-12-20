6a6ec1eb961927fd584f2529e912aafdcfdd61dd
LasramR <183598702+LasramR@users.noreply.github.com>
2024-12-20 15:55:10 +0100

    feat: added test for py command in install.ps1

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -1,4 +1,13 @@
-py -m venv "$($env:USERPROFILE)\\.mimic\\.venv"
+if (Get-Command py -ErrorAction SilentlyContinue) {
+    $PythonCmd = "py"
+} elseif (Get-Command python3 -ErrorAction SilentlyContinue) {
+    $PythonCmd = "python3"
+} else {
+    Write-Output "Neither 'py' launcher nor 'python3' is available in PATH."
+    exit 1
+}
+
+$PythonCmd -m venv "$($env:USERPROFILE)\\.mimic\\.venv"
 "$($env:USERPROFILE)\\.mimic\\.venv\\Scripts\\activate"
 pip install pipx
 pipx install -e "$($env:USERPROFILE)\\.mimic"
